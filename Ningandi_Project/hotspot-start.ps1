e81f0b9a6eb32c4b8c9f5e1fc5862b670fcdb1b5
Gandinga <saintsound@outlook.com>
2026-05-16 18:29:59 -0400

    Added argument input logic and fixed minor bug, made the printed messages more readable and removed dead code meant for future functions that are not nessesary

--- Ningandi_Project/hotspot-start.ps1
+++ Ningandi_Project/hotspot-start.ps1
@@ -1,38 +1,70 @@
-Add-Type -AssemblyName System.Runtime.WindowsRuntime
-$asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+#takes arguments for actions 
+param([string]$Action)
 
-Function Await($WinRtTask, $ResultType) {
+#gives me access to wndows system libraries
+Add-Type -AssemblyName System.Runtime.WindowsRuntime
+
+#converts WinRT async operations to .NET tasks for PowerShell
+Function Await($WinRtTask, $ResultType) 
+{
+    $asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
     $asTask = $asTaskGeneric.MakeGenericMethod($ResultType)
     $netTask = $asTask.Invoke($null, @($WinRtTask))
     $netTask.Wait(-1) | Out-Null
     $netTask.Result
 }
 
-Function AwaitAction($WinRtAction) {
-    $asTask = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and !$_.IsGenericMethod })[0]
-    $netTask = $asTask.Invoke($null, @($WinRtAction))
-    $netTask.Wait(-1) | Out-Null
-}
+#core hotspot function
+Function SetHotspot($Enable) 
+{
+    $connectionProfile = [Windows.Networking.Connectivity.NetworkInformation,Windows.Networking.Connectivity,ContentType=WindowsRuntime]::GetInternetConnectionProfile()      #grab current network connection and save it in connection profile
+    $tetheringManager = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager,Windows.Networking.NetworkOperators,ContentType=WindowsRuntime]::CreateFromConnectionProfile($connectionProfile)   #get hotspot controller object
 
-Function SetHotspot($Enable) {
-    $connectionProfile = [Windows.Networking.Connectivity.NetworkInformation,Windows.Networking.Connectivity,ContentType=WindowsRuntime]::GetInternetConnectionProfile()
-    $tetheringManager = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager,Windows.Networking.NetworkOperators,ContentType=WindowsRuntime]::CreateFromConnectionProfile($connectionProfile)
-
-    if ($Enable -eq 1) {
-        if ($tetheringManager.TetheringOperationalState -eq 1) {
+    if ($Enable -eq 1) 
+    {
+        if ($tetheringManager.TetheringOperationalState -eq 1) 
+        {
             "Hotspot is already On!"
-        } else {
-            "Hotspot is off! Turning it on"
+        } 
+        else 
+        {
+            "Turning hotspot on..."
             Await ($tetheringManager.StartTetheringAsync()) ([Windows.Networking.NetworkOperators.NetworkOperatorTetheringOperationResult])
         }
-    } else {
-        if ($tetheringManager.TetheringOperationalState -eq 0) {
+    } 
+    
+    else 
+    {
+        if ($tetheringManager.TetheringOperationalState -eq 2) 
+        {
             "Hotspot is already Off!"
-        } else {
-            "Hotspot is on! Turning it off"
+        } 
+        
+        else 
+        {
+            "Turning hotspot off..."
             Await ($tetheringManager.StopTetheringAsync()) ([Windows.Networking.NetworkOperators.NetworkOperatorTetheringOperationResult])
         }
     }
 }
+#forced the arguments to be uppercase internally
+$Action = $Action.ToUpper()
 
-SetHotspot(1)
+#use arguments to turn on or off the hotspot
+if ($Action -eq "ON") 
+{
+    SetHotspot(1)
+} 
+
+elseif ($Action -eq "OFF") 
+{
+    SetHotspot(0)
+} 
+#usage instructions if the parameter argument is wrong
+else 
+{
+    Write-Host "Usage:"
+    Write-Host "Turn on hotspot:  .\hotspot-start.ps1 on"
+    Write-Host "Turn off hotspot: .\hotspot-start.ps1 off"
+}
+
